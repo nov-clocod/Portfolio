8c4dd03be76dcc13691aa91e7a1aac5c80126e55
Sthealth <AW@example.com>
2025-10-30 10:41:42 -0400

    Create valuable interface and abstract fixedasset class

---
+++ src/main/java/com/pluralsight/classes/FixedAsset.java
@@ -0,0 +1,30 @@
+package com.pluralsight.classes;
+
+import com.pluralsight.interfaces.Valuable;
+
+public abstract class FixedAsset implements Valuable {
+    private String name;
+    private double value;
+
+    public FixedAsset(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
