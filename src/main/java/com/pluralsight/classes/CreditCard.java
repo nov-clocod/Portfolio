3ca4580c254389a43560893ea1ee0646fc8d9d5a
Sthealth <AW@example.com>
2025-10-30 11:10:32 -0400

    add CreditCard class implementing Valuable

---
+++ src/main/java/com/pluralsight/classes/CreditCard.java
@@ -0,0 +1,54 @@
+package com.pluralsight.classes;
+
+import com.pluralsight.interfaces.Valuable;
+
+public class CreditCard implements Valuable {
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    public CreditCard(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void charge(double amount) {
+        double amountCharged = balance + amount;
+        System.out.println("You were charged $" + amountCharged);
+    }
+
+    public void pay(double amount) {
+        double amountPaid = balance - amount;
+        System.out.println("You paid off $" + amountPaid);
+    }
+
+    @Override
+    public double getValue() {
+        return balance > 0 ? -balance : Math.abs(balance);
+    }
+}
